502e2d351bb64a2de20536d96adb53a3c3819436
Ti1mmy <47431121+Ti1mmy@users.noreply.github.com>
2022-12-09 00:00:14 -0500

    Finalize pixmap Python scripts

---
+++ scripts/bitmap.py
@@ -0,0 +1,60 @@
+#!/bin/python3
+from pixelator import Pixelator
+from PIL import Image, ImageOps
+import os 
+import numpy as np
+
+def make_bitmap(filename: str, width: int, height: int, palette_dict: dict, padding=0) -> None:
+    
+    pixelart_resolution = (width, height)
+    palette = list(palette_dict.keys())
+
+    png = Image.open(f'{filename}').convert('RGBA')
+    filename = filename.split('/')[-1]
+    
+    # Make border larger to shrink the image
+    png = ImageOps.expand(png, border=padding, fill=None)
+    background = Image.new('RGBA', png.size, (0,255,0)) # Make background green
+    png_masked = Image.alpha_composite(background, png)
+
+    # Should do nearest resample to prevent dithering
+    png_masked = png_masked.resize(pixelart_resolution, resample=Image.NEAREST)
+    png_masked.save(f'./out/{filename.split(".")[0]}_temp_masked.png')
+
+    img = Pixelator(filename=f'./out/{filename.split(".")[0]}_temp_masked.png')
+    pixelated_img = img.pixelate(
+        width = pixelart_resolution[0],
+        height = pixelart_resolution[1],
+        palette=palette
+    )
+
+    # Write pixelated image data to file
+    out_filename = filename.split('.')[0]
+
+    with open(f"out/{out_filename}_pixmap.h", "w") as out:
+        # Include Guard!!
+        out.write(f'#ifndef __{out_filename.upper()}_PIXMAP_H__\n')
+        out.write(f'#define __{out_filename.upper()}_PIXMAP_H__\n\n')
+        
+        # Define the pixmap
+        # We need to provide space for the null terminator (hence the width + 1)
+        out.write(f'static const char {out_filename}[{height}][{width + 1}] = ' + '{\n')
+        for horizontal_line in pixelated_img.data:
+            line = []
+            for pixel in horizontal_line:
+                pixel_tuple = tuple([int(x) for x in pixel])
+                pixel_colour = palette_dict[pixel_tuple]
+                line.append(pixel_colour)
+            line_str = '\t"' + f'{"".join(line)}' + '",\n'
+            out.write(line_str)
+        out.write('};\n\n')
+        out.write('#endif\n')
+        out.close()
+
+        #print(pixelated_img.data)
+        #pixelated_img.write(filename=f'./out/{image.split(".")[0]}_pixelated_80x80.png', height=80, width=80)
+            
+        # Comment this line out if you want to see what the masked image looks like
+        # In particular, could be useful if you decide to use it to bitmap-ify 
+        # other assets/videos
+        os.remove(f'./out/{out_filename}_temp_masked.png')
