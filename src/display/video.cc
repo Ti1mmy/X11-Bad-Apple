84c1c85eec60938e8c9acbbccd8c7030c7596073
Ti1mmy <47431121+Ti1mmy@users.noreply.github.com>
2022-12-09 23:49:48 -0500

    optimization

--- src/display/video.cc
+++ src/display/video.cc
@@ -4,7 +4,7 @@ using namespace std;
 Video::Video(const vector<const vector<string>*> &frames, int numFrames, int height, int width):
             frameWidth{width}, frameHeight{height}, currentFrame{0} {
                 for (int i = 0; i < numFrames; ++i) {
-                    frameList.emplace_back(frames[i]);
+                    frameList.emplace_back(move(frames[i]));
             }
         }
 
